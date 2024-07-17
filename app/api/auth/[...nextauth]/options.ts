060a8eb1e639211610dc6192042eb36416fe6185
neeramrutia <neer.amrutia@gmail.com>
2024-07-17 12:44:32 +0530

    added coordinator

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -119,7 +119,7 @@ export const options: NextAuthOptions = {
                         email : profile?.email,
                         name : profile?.name,
                         password : makeid(10),
-                        role : "Coordinator",
+                        role : "coordinator",
                         isCoordinator : true,
                         isAdmin : false
                     })
